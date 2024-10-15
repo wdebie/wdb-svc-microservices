c2710327dd4b2ca93c9fc0e474af187dfc38d56e
Seppe Van Campfort <r0929446@student.thomasmore.be>
2024-10-15 14:53:33 +0200

    modules toegevoegd

---
+++ stage-service/src/test/java/com/example/stageservice/StageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
