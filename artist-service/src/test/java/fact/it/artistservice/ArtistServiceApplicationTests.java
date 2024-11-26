cc762b89d871e5891cc23607fe40e534e4608879
Willem De Bie <debiewillem@outlook.com>
2024-11-26 13:12:16 +0100

    Update artist-service tests

--- artist-service/src/test/java/fact/it/artistservice/ArtistServiceApplicationTests.java
+++ artist-service/src/test/java/fact/it/artistservice/ArtistServiceApplicationTests.java
@@ -2,9 +2,14 @@ package fact.it.artistservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import fact.it.artistservice.repository.ArtistRepository;
 
 @SpringBootTest
 class ArtistServiceApplicationTests {
+
+    @MockBean
+    private ArtistRepository artistRepository;
 
     @Test
     void contextLoads() {
