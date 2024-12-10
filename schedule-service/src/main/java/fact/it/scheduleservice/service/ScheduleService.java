8f1f70b20b17c458fdfbeeb91cda08dab5a55743
Seppe Van Campfort <r0929446@student.thomasmore.be>
2024-12-10 17:59:53 +0100

    it works

--- schedule-service/src/main/java/fact/it/scheduleservice/service/ScheduleService.java
+++ schedule-service/src/main/java/fact/it/scheduleservice/service/ScheduleService.java
@@ -4,15 +4,43 @@ import fact.it.scheduleservice.dto.ScheduleRequest;
 import fact.it.scheduleservice.dto.ScheduleResponse;
 import fact.it.scheduleservice.model.Schedule;
 import fact.it.scheduleservice.repository.ScheduleRepository;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
+
+    @PostConstruct
+    public void loadData() {
+        if (scheduleRepository.count() <= 0) {
+            Schedule schedule1 = Schedule.builder()
+                    .skuCode("event123")
+                    .startTime(LocalDateTime.of(2023, 10, 1, 10, 0))
+                    .endTime(LocalDateTime.of(2023, 10, 1, 12, 0))
+                    .ArtistId("1")
+                    .StageId(2L)
+                    .FoodTruckId(1L)
+                    .build();
+
+            Schedule schedule2 = Schedule.builder()
+                    .skuCode("event456")
+                    .startTime(LocalDateTime.of(2023, 10, 2, 14, 0))
+                    .endTime(LocalDateTime.of(2023, 10, 2, 16, 0))
+                    .ArtistId("2")
+                    .StageId(1L)
+                    .FoodTruckId(2L)
+                    .build();
+
+            scheduleRepository.save(schedule1);
+            scheduleRepository.save(schedule2);
+        }
+    }
 
     public void createSchedule(ScheduleRequest scheduleRequest){
         Schedule schedule = Schedule.builder()
