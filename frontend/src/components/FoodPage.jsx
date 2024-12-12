b7e96d6ff90e5896c28c0726b952717c62be383a
wlinator <git@wlinator.org>
2024-12-12 11:38:31 -0500

    A

--- frontend/src/components/FoodPage.jsx
+++ frontend/src/components/FoodPage.jsx
@@ -8,7 +8,7 @@ function FoodPage() {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        axios.get('/api/fooditems')
+        axios.get('/fooditems', { baseURL: 'https://api.fritfest.com', secure: true, changeOrigin: true })
             .then(response => {
                 const items = response.data;
                 setFoodItems(items);
