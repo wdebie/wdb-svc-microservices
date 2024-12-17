bbac406dd9f627b9a297cab3ea87436f273dea82
Willem De Bie <debiewillem@outlook.com>
2024-12-17 10:22:20 -0500

    Get frontend Google information from .env

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google"
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId='661879216618-0gl8krhgkntc04h7m8qfvnp3k16datne.apps.googleusercontent.com'>
+  <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_ID}>
     <StrictMode>
       <BrowserRouter>
         <App />
